c34797551f7cdd56fab21e3b1f4f72413e9f7683
Richard Patching <richard@justaddpixels.com>
2011-10-09 16:27:57 +0100

    Refactor magic numbers into more meaningful methods.

--- lib/dungeon.rb
+++ lib/dungeon.rb
@@ -9,6 +9,8 @@ end
 
 class Dungeon
   attr_reader :tiles, :width, :height, :cells
+  CELLS = 5
+
   def initialize(width, height, seed=nil)
     @tiles = Array.new(height) { |y| Array.new(width) { |x| Tile.new(x,y)}}
     @width = width
@@ -16,13 +18,21 @@ class Dungeon
     @cells = []
   end
 
+  def max_cell_width
+    CELLS - 1
+  end
+
+  def max_cell_height
+    max_cell_width
+  end
+
   def build_grid
-    (0..35).step(5) do |x|
-      (0..15).step(5) do |y|
+    (0..35).step(CELLS) do |x|
+      (0..15).step(CELLS) do |y|
         point_1 = [@tiles[x][y].y, @tiles[x][y].x]
-        point_2 = [@tiles[x+4][y].y, @tiles[x+4][y].x]
-        point_3 = [@tiles[x][y+4].y, @tiles[x][y+4].x]
-        point_4 = [@tiles[x+4][y+4].y, @tiles[x+4][y+4].x]
+        point_2 = [@tiles[x+max_cell_width][y].y, @tiles[x+max_cell_width][y].x]
+        point_3 = [@tiles[x][y+max_cell_height].y, @tiles[x][y+max_cell_height].x]
+        point_4 = [@tiles[x+max_cell_width][y+max_cell_height].y, @tiles[x+max_cell_width][y+max_cell_height].x]
         @cells << Cell.new(point_1, point_2, point_3, point_4)
       end
     end
